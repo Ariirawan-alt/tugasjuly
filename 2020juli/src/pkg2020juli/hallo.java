45b40cc5f8fe434c62d75d07b12deb56efb4191c
Ariirawan-alt <68743217+Ariirawan-alt@users.noreply.github.com>
2020-07-24 21:34:51 +0700

    Add files via upload

---
+++ 2020juli/src/pkg2020juli/hallo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2020juli;
+
+/**
+ *
+ * @author User
+ */
+class hallo {
+    public static void main(String[] args) {
+        
+    }
+}
